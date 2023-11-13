ce9751f34275825a8bc42a1ffa54ed001230dd10
Erkka Pyykonen <erkka.pyykonen@gmail.com>
2023-11-14 00:08:47 +0900

    fix code

--- reactsignalsdemo/src/app/Sidebar.tsx
+++ reactsignalsdemo/src/app/Sidebar.tsx
@@ -2,27 +2,16 @@ import { count } from "./page";
 
 export const Sidebar = () => {
   return (
-    <div>
+    <div className="border border-black h-[1000px] m-1 w-[300px] text-center">
       <div>Sidebar Count : {count}</div>
       <div>
-        <button onClick={() => count.value++}>Increase count</button>
+        <button
+          className="bg-slate-200 p-1 border border-black"
+          onClick={() => count.value++}
+        >
+          Increase count
+        </button>
       </div>
     </div>
   );
 };
-
-// export const Sidebar = () => {
-//   return (
-//     <div className="border border-black h-[1000px] m-1 w-[300px] text-center">
-//       <div>Sidebar Count : {count}</div>
-//       <div>
-//         <button
-//           className="bg-slate-200 p-1 border border-black"
-//           onClick={() => count.value++}
-//         >
-//           Increase count
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
